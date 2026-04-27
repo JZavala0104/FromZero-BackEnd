8615e5a9c2439176f1a051e95cb8c8b7e23d7b4e
jose_ <u202219184@upc.edu.pe>
2026-04-27 14:25:54 -0500

    Creación Entities/Repository/ServiceInterface/ServiceImplements/DTO.

---
+++ src/test/java/pe/edu/upc/fromzero/FromZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.fromzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromZeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
